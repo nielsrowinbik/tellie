cebc393e90dee03646bdc33b25d2d47334afa606
Niels <nielsrowinbik@users.noreply.github.com>
2019-01-05 19:26:08 +0100

    Move to express and add support for serving static files

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import * as dotenv from 'dotenv';
 import * as glob from 'glob';
 import * as ngrok from 'ngrok';
 import * as path from 'path';
-import Telegraf from 'telegraf';
+import Telegraf, { ContextMessageUpdate } from 'telegraf';
 import * as commandParts from 'telegraf-command-parts';
 import * as ora from 'ora';
+import * as express from 'express';
+import routes from './routes';
 
 dotenv.config();
 
@@ -14,25 +16,26 @@ const init = async () => {
     const path = process.env.BOT_PATH || '';
     const port = process.env.BOT_PORT ? parseInt(process.env.BOT_PORT, 10) : 80;
     const url = process.env.BOT_URL || (await ngrok.connect(port));
+
     const bot = new Telegraf(process.env.BOT_TOKEN || '', {
         telegram: {
             webhookReply: false,
         },
         username: process.env.BOT_NAME || '',
     });
-
-    // TODO: Fix errors caused by line below in production build
     bot.telegram.setWebhook(`${url}/${path}`);
     bot.use(commandParts());
-
     setupHandlers(bot);
 
-    bot.startWebhook(`/${path}`, null, port);
+    const app = express();
+    app.use(bot.webhookCallback(`/${path}`));
+    setupStaticRoutes(app);
 
+    await app.listen(port);
     spinner.succeed(`Done. Listening on ${url}/${path}`);
 };
 
-const setupHandlers = (bot: any) => {
+const setupHandlers = (bot: Telegraf<ContextMessageUpdate>) => {
     const files = glob.sync('/commands/*.ts', {
         root: path.resolve(__dirname),
     });
@@ -46,6 +49,12 @@ const setupHandlers = (bot: any) => {
         } catch (error) {
             spinner.fail(`Could not load command from ${file}`);
         }
+    });
+};
+
+const setupStaticRoutes = app => {
+    routes.forEach(({ route, folder }) => {
+        app.use(route, express.static(['static', folder].join('/')));
     });
 };
 
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+const routes = [
+    {
+        route: '/datepicker/:id',
+        folder: 'datepicker',
+    },
+];
+
+export default routes;
