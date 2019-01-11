febe0e76638a3fe9f3279f78ffd98e8d46a97f91
Niels <nielsrowinbik@users.noreply.github.com>
2019-01-11 13:41:18 +0100

    Minor changes to define

--- src/commands/define.ts
+++ src/commands/define.ts
@@ -1,32 +1,38 @@
 import { Extra } from 'telegraf';
 import fetch from 'node-fetch';
-import { get } from 'lodash';
+import { capitalize, get, lowerCase } from 'lodash';
 
 const DefineCommand = async ({
     message: { message_id },
     reply,
     state: { command },
 }: any) => {
-    const options = Extra.markdown().inReplyTo(message_id);
-    const { splitArgs: words } = command;
-    const word = words[0];
+    const defaultOptions = Extra.markdown().inReplyTo(message_id);
+    const { args: words } = command;
 
     const response: any = await (await fetch(
-        `https://api.urbandictionary.com/v0/define?term=${word}`
+        `https://api.urbandictionary.com/v0/define?term=${words}`
     )).json();
     const result = get(response, 'list[0]');
 
     if (result === null || result === undefined)
         return reply(
-            `Sorry, I could not find a definition for *${word}*`,
-            options
+            `Sorry, I could not find a definition for *${words}*.`,
+            defaultOptions
         );
 
-    const { author, definition } = result;
+    const { author, definition, permalink, word } = result;
+    const extraOptions = defaultOptions.markup(m =>
+        m.inlineKeyboard([
+            [m.urlButton('View definition on the web', permalink)],
+        ])
+    );
 
     return reply(
-        `According *${author}* on Urban Dictionary, the definition of *${word}* is:\n\n_${definition}_`,
-        options
+        `According *${author}* on Urban Dictionary, the definition of *${lowerCase(
+            word
+        )}* is:\n\n_${capitalize(definition)}_`,
+        extraOptions
     );
 };
 
